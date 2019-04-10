ffe1c9b272b5c4ca2a70dceb2d6ca108d33126e1
Jason Lindsey <jasonlindsey@Jasons-MacBook-Pro.local>
2019-04-10 15:06:05 -0500

    first commit

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,18 +1,32 @@
 "use strict";
 
-var input = '';
 
-   while(input<1 || input>50 || input % 2 == 0){
+
+    var input = '';
+
+    while (input < 1 || input > 50 || input % 2 == 0) {
         input = prompt('please enter an odd number between 1 and 50')
-   }
+    }
+    console.log('the number to skip is: ' + input);
 
-   console.log('the number to skip is: ' + input);
+    /*
 
-   for(var i=1; i<50; i++){
-        if (i == input){
-            console.log('yikes! skipping number: ' + input)
-        } else if (i % 2 == 0) {
-            continue
+     do {
+         input = prompt('please enter an odd number between 1 and 50')
+     } while(input<1 || input>50 || input % 2 == 0);
+
+    console.log('the number to skip is: ' + input);*/
+
+
+    for (var i = 1; i < 50; i++) {
+        if (i % 2 === 0) {
+            continue;
+        } else if (i == input) {
+            console.log('Yikes! skipping number: ' + i);
+        } else {
+            console.log('Here is an odd number: ' + i);
         }
-        console.log('Here is an odd number: ' + i)
-   }
+    }
+
+
+
--- js/for_loops.js
+++ js/for_loops.js
@@ -1,20 +1,54 @@
+"use strict";
+
+
 function showMultiplicationTable(num) {
     for(var i = 1; i <= 10; i++){
         console.log(num + " x " + i + " = " + (num * i));
     }
 }
+/*
+
+function outputRow (a, b) {
+    var output = '';
+    var total = a * b;
+    output += a + 'x' + b + ' = ' + total;
+    return output;
+}
+
+function showMultiplicationTable2 (num) {
+    var output = '';
+    for (var j=1; j<=10; j++){
+        output += outputRow(num, j) + '\n'
+    }
+    return output;
+}
+
+console.log(showMultiplicationTable2(7));
+*/
 
 showMultiplicationTable(2);
-showMultiplicationTable(10);
+showMultiplicationTable(47);
 
+function randNum(min, max) {
+    return Math.floor(Math.random() * (max - min +1) + min)
+}
+
+
+for (var i =1; i<10; i++) {
+    var output = '';
+    for (var j=0; j < i; j++){
+        output += i
+    }
+    console.log(output);
+}
 
 
 for(var i = 1; i<=10; i++) {
-    var randNum = Math.floor((Math.random()*10) + 1);
-    if (randNum%2===0) {
-        console.log(randNum + " is even");
+    var num = randNum(20, 200);
+    if (num%2===0) {
+        console.log(num + " is even");
     } else {
-        console.log(randNum + ' is odd');
+        console.log(num + ' is odd');
     }
 }
 
@@ -24,10 +58,14 @@ for(var i = 1; i<10; i++) {
     console.log(j * i);
 }
 
-/* */
+for(var i = 1; i<10; i++) {
+    console.log(String(i).repeat(i));
+}
+
 
 
 for (var i = 100; i>=5; i-=5){
     console.log(i)
 }
+
 
