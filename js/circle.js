285488ee8241c79095eca87ef51ebcc3e7e3d976
Jason Lindsey <jasonlindsey@Jasons-MacBook-Pro.local>
2019-04-12 14:38:07 -0500

    done

--- js/circle.js
+++ js/circle.js
@@ -27,8 +27,8 @@
                 console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea(circle.radius));
 
             }
-        },
-    }
+        }
+    };
 
         // log info about the circle
         console.log("Raw circle information");
@@ -37,10 +37,10 @@
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
 
-    console.log("=======================================================");
+    console.log("================================================");
     // TODO: Change the radius of the circle to 5.
 
-    circle.radius = 5
+    circle.radius = 5;
 
     // log info about the circle
     console.log("Raw circle information");
