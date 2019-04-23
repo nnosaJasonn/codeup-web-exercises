d66688ef18cd7f04fcda3d718d001f5f833cae9e
Jason Lindsey <jasonlindsey@Jasons-MacBook-Pro.local>
2019-04-23 10:13:31 -0500

    begining jquery

---
+++ js/jquery_exercises.js
@@ -0,0 +1,14 @@
+/*$(document).ready(function(){
+    alert('theDOM has finished loading!')
+});*/
+var contents = $('#nonagon').html();
+alert(contents);
+
+var h1Alert = $('h1').html();
+alert(h1Alert);
+
+$('.codeup').css('border', '1px solid red');
+
+$('li').css('font-size', '20px');
+
+$('h1, p, li').css('background', 'yellow');
