89708862ea5955e74c8a76f53b92bd2db33717eb
Jason Lindsey <jasonlindsey@Jasons-MacBook-Pro.local>
2019-04-24 14:51:00 -0500

    jquery faq

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,5 +1,20 @@
-var playerInput = 'X';
+var player1Input = 'X';
+var player2Input = 'O';
+
+var winningOptions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0,3,6],
+    [1, 4, 7],
+    [2, 5, 8]
+];
+
+$('#start').click(function(){
+
+});
 
 $('.tic').click(function(){
-   $(this).html(playerInput)
+   $(this).html(player1Input)
+
 });
