c87f07c93b28205eda6f41458d2a11f99b6481d8
Jason Lindsey <jasonlindsey@Jasons-MacBook-Pro.local>
2019-05-03 09:43:15 -0500

    weather project

--- js/mapbox-geocoder-utils.js
+++ js/mapbox-geocoder-utils.js
@@ -51,6 +51,6 @@ function reverseGeocode(coordinates, token) {
         })
         // to get all the data from the request, comment out the following three lines...
         .then(function(data) {
-            return data.features[0].place_name;
+            return data.features[0]
         });
 }
